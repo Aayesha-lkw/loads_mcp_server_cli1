73d61988c46ba6b46a44ec26cc7f198e18e8b0b9
Aayesha-lkw <aayesha@walter-group.com>
2025-10-23 09:17:46 +0000

    updated work repo

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from google.adk.agents import LlmAgent
 from google.adk.runners import Runner
 from google.adk.sessions import InMemorySessionService
-from mcp.server.fastmcp import FastMCP
 from google.genai import types
+from mcp.server.fastmcp import FastMCP
 import requests
 import pandas as pd
 from dotenv import load_dotenv
@@ -33,7 +33,7 @@ input_other_agent = LlmAgent(
     output_key="query_other"
 )
 root_agent = input_other_agent
-runner_walter = Runner(app_name=APP_NAME, agent=root_agent, session_service=session_service)
+runner = Runner(app_name=APP_NAME, agent=root_agent, session_service=session_service)
 
 @mcp.tool()
 async def get_about_walter(user_text:str) -> str:
@@ -45,7 +45,7 @@ async def get_about_walter(user_text:str) -> str:
     await session_service.create_session(app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID)
     content = types.Content(role='user', parts=[types.Part(text=user_text)])
     full_response_text = []
-    async for event in runner_walter.run_async(user_id=USER_ID, session_id=SESSION_ID, new_message=content):
+    async for event in runner.run_async(user_id=USER_ID, session_id=SESSION_ID, new_message=content):
         if event.is_final_response() and event.content and event.content.parts:
             response = ''.join([part.text for part in event.content.parts if part.text])
             if response:
